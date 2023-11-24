e665598e53c558afd85425194ffaa5cb810879b5
Vitória <vick_santos_vick@hotmail.com>
2023-11-24 01:51:16 -0300

    adicionando

---
+++ lib/core/paletadecores.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaletaDeCores {
+  static const Color background = Color(0xFFEFF5FF);
+  static const Color card = Colors.white;
+  static const Color title = Color(0xFFCD8646);
+  static const Color backgroundinput = Color(0xFFE7E1D1);
+  static const Color button = Color(0xFFA4A4C8);
+}
