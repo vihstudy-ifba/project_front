4199b53bcc64c643b4946d5f421d91f7f6ecb158
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-11-27 23:00:01 -0300

    --- adicionando método de criar usuário

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,3 +1,25 @@
+import 'package:project_front/core/routes.dart';
+import 'package:project_front/models/usuario_model.dart';
+import 'package:http/http.dart' as http;
+
 class UserRepository{
   
+
+
+  Future<bool?> criarUsuario(UsuarioModel usuarioModel) async {
+    try {
+      String url = Routes.novousuario;
+
+      var response = await http.post(Uri.parse(url), body: usuarioModel.toJson(), headers: {
+        "content-type": "application/json",
+        "accept": "application/json",
+      });
+
+      if (response.statusCode == 200) return true;
+
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
 }
